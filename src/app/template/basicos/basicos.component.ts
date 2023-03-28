790350bc72b64874d75dc6a59aa4a4855dcfd64f
omar.lopez <contacto.omarlopez@gmail.com>
2023-03-28 16:52:46 -0400

    Form App V0.0.1

---
+++ src/app/template/basicos/basicos.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-basicos',
+  templateUrl: './basicos.component.html',
+  styleUrls: ['./basicos.component.css']
+})
+export class BasicosComponent {
+
+  save(){
+    console.log("Save Form");
+  }
+
+}
